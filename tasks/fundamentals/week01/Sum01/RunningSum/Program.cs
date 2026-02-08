2a884acbf81687d9140ec5ae05bd9d828fb6c539
kirsty-evans <kirstysevans@gmail.com>
2026-02-08 11:01:43 +1100

    Sum01 attempt 1. Fails test

--- tasks/fundamentals/week01/Sum01/RunningSum/Program.cs
+++ tasks/fundamentals/week01/Sum01/RunningSum/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("This program will maintain a running sum until there is no more data.\n");
+
+int runningSum = 0;
+string? input;
+
+while (true) {
+
+    Console.Write("Enter a number: ");
+    input = Console.ReadLine();
+
+
+
+    // will end the program is input is empty
+    if (string.IsNullOrEmpty(input)) {
+            break;
+        }
+
+    // checks input is a valid integer
+    // if so, it saves it to 'number'
+    if (int.TryParse(input, out int number))
+        {
+            runningSum += number;
+        }
+
+    }
+
+Console.WriteLine($"\nThe total is: {runningSum}");
+

3394e59f0320f2f8a8fd706ba7f712426bc6012c
kirsty-evans <kirstysevans@gmail.com>
2026-02-08 12:57:41 +1100

    finished week01

--- tasks/fundamentals/week01/EvenOrOdd05/EvenOrOdd/Program.cs
+++ tasks/fundamentals/week01/EvenOrOdd05/EvenOrOdd/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+        string input = Console.ReadLine();
+
+        // Test is expecting newline to be Odd
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Odd");
+        }
+        
+        else if (int.TryParse(input, out int number))
+        {
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }
+

25e73bf0c7855671bf65e0221601682e7c4a0943
kirsty-evans <kirstysevans@gmail.com>
2026-02-15 18:22:38 +1100

    01 done

--- tasks/fundamentals/week02/Searching01/WordSearch/WordSearcher.cs
+++ tasks/fundamentals/week02/Searching01/WordSearch/WordSearcher.cs
@@ -4,12 +4,24 @@ public class WordSearcher
 {
 
     public static string? Find(string[] words, string word) {
+
+        for (int i = 0; i < words.Length; i++) {
+            if (words[i] == word) {
+                return words[i];
+            }
+        }
         
         return null;
 
     }
 
     public static int? FindIndex(string[] words, string word) {
+
+        for (int i = 0; i < words.Length; i++) {
+            if (words[i] == word) {
+                return i;
+            }
+        }
         return null;
     }
 

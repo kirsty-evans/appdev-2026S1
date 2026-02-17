1b124a4cc56e74c68414876a281c27b79f2c5872
kirsty-evans <kirstysevans@gmail.com>
2026-02-17 20:37:44 +1100

    adding arrays solution

--- tasks/fundamentals/week02/AddingArrays04/AddingArrays/AddingArrays.cs
+++ tasks/fundamentals/week02/AddingArrays04/AddingArrays/AddingArrays.cs
@@ -1,12 +1,33 @@
-﻿namespace AddingArrays;
+﻿using System.Buffers;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices.Marshalling;
+using System.Xml.Serialization;
+
+namespace AddingArrays;
 
 public class AddingArrays
 {
 
     public static int[] Join(int[] arrayA, int[] arrayB) {
 
+        //initialise new array
 
-        return new int[1];
+        int newArrayLength = arrayA.Length + arrayB.Length;
+        int[] newArray = new int[newArrayLength];
+
+        for (int i = 0; i < arrayA.Length; i++)
+        {
+            newArray[i] = arrayA[i];
+        }
+
+        for (int i = 0; i < arrayB.Length; i++)
+        {
+            newArray[i + arrayA.Length] = arrayB[i];
+        }
+
+        return newArray;
     }
 
 }

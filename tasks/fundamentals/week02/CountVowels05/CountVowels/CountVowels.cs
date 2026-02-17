ee46733370c5d2bd2c3e983eb2e97673f522b105
kirsty-evans <kirstysevans@gmail.com>
2026-02-17 21:46:39 +1100

    count vowels solution

--- tasks/fundamentals/week02/CountVowels05/CountVowels/CountVowels.cs
+++ tasks/fundamentals/week02/CountVowels05/CountVowels/CountVowels.cs
@@ -5,7 +5,39 @@ public class CountVowels
 
     public static int[] Count(string line) {
 
-        return new int[5];
+        int [] VowelArray = new int[5];
+
+        if (line == null) {
+            return VowelArray;
+        }
+
+        if (line.Length == 0) {
+            return VowelArray;
+        }
+
+        if (line.Length > 0) {
+            foreach (char c in line) {
+                switch (char.ToLower(c)) {
+                    case 'a':
+                        VowelArray[0]++;
+                        break;
+                    case 'e':
+                        VowelArray[1]++;
+                        break;
+                    case 'i':
+                        VowelArray[2]++;
+                        break;
+                    case 'o':
+                        VowelArray[3]++;
+                        break;
+                    case 'u':
+                        VowelArray[4]++;
+                        break;
+                }
+            }
+        }
+
+        return VowelArray;
     }
 
 }

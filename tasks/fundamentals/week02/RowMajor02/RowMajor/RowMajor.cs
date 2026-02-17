8387784e4990e6aff7e2c9a4bfea38e8cbe500e8
kirsty-evans <kirstysevans@gmail.com>
2026-02-17 18:53:24 +1100

    row major solved

--- tasks/fundamentals/week02/RowMajor02/RowMajor/RowMajor.cs
+++ tasks/fundamentals/week02/RowMajor02/RowMajor/RowMajor.cs
@@ -8,15 +8,28 @@ public class RowMajorArray
     public int Height { get; private set; }
 
     public RowMajorArray(int[] array, int width, int height) {
-
+            this.array = array;
+            this.Width = width;
+            this.Height = height;
     }
 
     public int Get(int x, int y) {
+        //  Retrieves an element from the array using x, y coordinates. 
+        //  Given an array where there are 3 rows and 10 elements per a row, calling Get(5, 1) will get the element at index 15.
+        
 
-        return 0;
+        // working out the formula..
+        // (5,0) index would be 5
+        // (5,1) index would be 15
+        // (5,2) index would be 25
+        // so the formula is x + (y*width)
+
+        return this.array[y*this.Width + x];
+
     }
 
-    public void Set(int x, int y, int v) {
+    // public void Set(int x, int y, int v) {
+
 
     }
-}
+

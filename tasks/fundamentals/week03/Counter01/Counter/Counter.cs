e7a2b51e7b9f12f0244d836b05d4b496016e8ab9
kirsty-evans <kirstysevans@gmail.com>
2026-02-20 15:27:43 +1100

    Counter01 solution, 1 test case fails?

--- tasks/fundamentals/week03/Counter01/Counter/Counter.cs
+++ tasks/fundamentals/week03/Counter01/Counter/Counter.cs
@@ -1,36 +1,45 @@
 ﻿namespace Counter;
 
 public class Counter
+
 {
+
+	private int count;
+	private int originalStart;
 	public Counter() {
+		count = 0;
+		originalStart = 0;
 		
 	}
 
 	public Counter(int start)
 	{
+
+		count = start;
+		originalStart = start;
 		
 	}
 
 
 	public void Forward()
 	{
-		
+		count++;
 	}
 
 
 	public void Backward()
 	{
-		
+		count--;
 	}
 
 
 	public int Get()
 	{
-		return 0;
+		return count;
 	}
 
 	public void Reset()
 	{
-		
+		count = originalStart;
 	}
 }

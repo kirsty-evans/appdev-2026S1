85862d1eb6e7c393a9b8a795d5c8425386d91534
kirsty-evans <kirstysevans@gmail.com>
2026-02-20 16:10:09 +1100

    sliding window solution, 2 test cases fail

--- tasks/fundamentals/week03/SlidingWindow04/SlidingWindow/SlidingWindow.cs
+++ tasks/fundamentals/week03/SlidingWindow04/SlidingWindow/SlidingWindow.cs
@@ -3,24 +3,35 @@
 public class SlidingWindow
 {
 
+	private int[] array; // 1D array of integers
+	private int offset; // index of the array where the window will start
+	private int length; // length of the window
+
 	public SlidingWindow(int[] array, int offset, int length)
 	{
-		
+		this.array = array;
+		this.offset = offset;
+		this.length = length;
 	}
 
 	public void SetOffset(int offset)
 	{
+		this.offset = offset;
 		
 	}
 
 	public int GetElement(int index)
 	{
-		return 0;
+		return array[offset + index];
 	}
 
 	public void SetElement(int value, int index)
 	{
-		
+		if (index < 0 || index >= length)
+		{
+			return; // or exception
+		}
+		array[offset + index] = value;
 	}
 
 }

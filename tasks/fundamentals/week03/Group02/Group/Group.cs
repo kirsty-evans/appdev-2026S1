b3501a2270e7aeddf458e6c4c15b198da978c7ea
kirsty-evans <kirstysevans@gmail.com>
2026-02-20 20:16:06 +1100

    group02 answered

--- tasks/fundamentals/week03/Group02/Group/Group.cs
+++ tasks/fundamentals/week03/Group02/Group/Group.cs
@@ -3,39 +3,78 @@
 public class Group
 {
 
+	private int size;
+	private int currentMembers;
+	private	Member[] members;
 	public Group(int size)
 	{
+		this.size = size;
+		currentMembers = 0;
+		members = new Member[size];
+
 		
 	}
 
 	public bool AddMember(Member member)
 	{
+		if (currentMembers < size)
+			{
+				members[currentMembers] = member;
+				currentMembers++;
+				return true;
+			}
 
 		return false;
-	}
+			
+		}
+	
 
 	public bool RemoveMember(Member member)
 	{
+
+
+		for (int i = 0; i < currentMembers; i++)
+		{
+			if (members[i] == member)
+			{
+				members[i] = members[currentMembers - 1]; // Move the last member to the current position so there is no gap
+				members[currentMembers - 1] = null; // Clear the last position
+				currentMembers--;
+				return true;
+			}
+		}
 
 		return false;
 	}
 
 	public Member[] AllMembers()
 	{
+		Member[] allMembers = new Member[currentMembers];
+		for (int i = 0; i < currentMembers; i++)
+		{
+			allMembers[i] = members[i];
+		}
 
-		return new Member[1];
+		return allMembers;
 	}
 
 
 	public Member? GetMember(int memberNo)
 	{
+		for (int i = 0; i < currentMembers; i++)
+		{
+			if (members[i].GetMemberNo() == memberNo)
+			{
+				return members[i];
+			}
+		}
 		return null;
 	}
 
 
 	public int GroupSize()
 	{
-		return 0;
+		return currentMembers;;
 	}
 
 }
